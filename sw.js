813900e72a6d212a1e063ecd70803d29b9e2abf1
obesiteguidepratique <olivier@tyrbas.eu>
2026-05-28 05:15:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Alcool Barialimites — Service Worker
 // Version cache : à incrémenter à chaque mise à jour du HTML
-const CACHE_NAME = 'barialimites-b39';
+const CACHE_NAME = 'barialimites-b43';
 
 const ASSETS = [
   './index.html',
